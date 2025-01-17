d319856a7926115c8ff297507c08d814a017fc91
Brad <badz@bdesigns.net>
2025-01-17 18:43:58 -0500

    add files

---
+++ build/precache-manifest.e5c0d0dd9a75fd1d4545bf7e425d54b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411e66c56dfb95978e56bef97816319e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39c170d8b9bd2df1cab",
+    "url": "/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "5203d217bf6fe29a1791",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b39c170d8b9bd2df1cab",
+    "url": "/static/js/2.2a9429be.chunk.js"
+  },
+  {
+    "revision": "8e3a127b3eb410b6d841",
+    "url": "/static/js/3.445915d0.chunk.js"
+  },
+  {
+    "revision": "5203d217bf6fe29a1791",
+    "url": "/static/js/main.6c24f3bd.chunk.js"
+  },
+  {
+    "revision": "98743e54bceb60dc1568",
+    "url": "/static/js/runtime~main.9a4bcc00.js"
+  }
+]);
